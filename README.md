bad9f058caadf72c546b25ff6e07a01a62bac046
KrolKamil <kamilkrol1997@wp.pl>
2019-02-25 22:02:04 +0100

    last commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Projekt uniwersytecki
+
+## Opis programu
+Niech B Bedzie dodatnią liczbą całowitą. Liczbę naturalną n nazwiemy B-gładką, jeśli w jej  
+rozkładzie na czynniki pierwsze nie występują liczby pierwsze większe od B. Równoważnie  
+możemy powiedzieć, że liczbę n nazywamy B-gładką, gdy można przedstawić ją jako iloczyn  
+dodatnich liczb całkowitych mniejszych bądź równych B.  
+
+Program:  
+- wczyta ze standardowego wejścia trzy dodatnie liczby całkowite n,m oraz B
+- wyznaczy liczbę wszystkich liczb B-gładkich w przedziale [n,n+m](włącznie)  
+- wypisze wynik na standardowe wyjście  
+
+## Wejście
+
+W pierwszym wierszu wejścia zapisano trzy liczby całkowite n,m i B, podzielone pojedyńczmymi  
+odstępami, 1<=20000000, 1<=1000000, 1<=B<=10000
+
+## Wyjście
+
+Twój program powinien zapisać w pierwszym wierszu standardowego wyjścia jedną liczbę  
+całkowita - wyznaczoną liczbę liczb B-gładkich
---
+++ menu.bat
@@ -0,0 +1,88 @@
+@echo off
+
+:start
+echo ### MENU ###
+echo Wybierz ktory skrypt chesz wykonac:
+echo START - 1
+echo INFORMACJE - 2
+echo BACKUP - 3
+echo ZAKONCZ - 4
+set /p x=Wyprwoadz liczbe: 
+if %x%==1 GOTO a
+if %x%==2 GOTO b
+if %x%==3 GOTO c
+if %x%==4 GOTO exit
+cls
+GOTO start
+
+:a
+cls
+echo Prosze czekac az skrypt zakonczy prace
+IF NOT EXIST in (
+mkdir in
+)
+IF NOT EXIST out (
+mkdir out
+)
+SET in=%~dp0in\1i.txt
+SET out=%~dp0out\1o.txt
+start /wait java Main %in% %out%
+
+SET in=%~dp0in\2i.txt
+SET out=%~dp0out\2o.txt
+start /wait java Main %in% %out%
+
+SET in=%~dp0in\3i.txt
+SET out=%~dp0out\3o.txt
+start /wait java Main %in% %out%
+
+SET in=%~dp0in\4i.txt
+SET out=%~dp0out\4o.txt
+start /wait java Main %in% %out%
+
+start /wait web.py
+
+pause
+cls
+GOTO start
+
+:b
+cls
+echo 			***Liczby B-gladkie***
+echo.
+echo Program rozwiazuje zadanie konkursowe z algorytmionu
+echo.
+echo Algorytm ma za zadanie znajdywanie liczb B-gladkich na danych przedzialach
+echo.
+echo Przedzialy oraz liczba b jest podawana w pierwszej linicje pliku
+echo.
+echo Wyniki pracy algorytmu zostaja opisane w witrynie internetowej
+echo.
+echo Szczegolowy opis rozwiazania znajduje sie w dokumetacji
+echo.
+
+pause
+cls
+GOTO start
+
+:c
+cls
+echo Backup
+
+SET mypath=%~dp0
+SET source=%mypath:~0,-1%
+cd..
+mkdir backup
+SET dest=%cd%\backup
+cd %source%
+xcopy %source% %dest% /s /i
+
+pause
+cls
+GOTO start
+
+:exit
+cls
+echo Koniec
+pause
+echo on
